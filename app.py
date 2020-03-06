c095c174e5ee02a291c22bc90437a5ccdb94f0a4
Shandy Gunawan <shandy.gunawan@rocketmail.com>
2020-03-07 00:36:18 +0700

    first commit. Interface for index imported from website project

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/result', methods=["POST"])
+def result():
+    print(request.form)
+    return "Hello World!"
+
+
+if __name__ == '__main__':
+    app.run()
